857c79927c1b29fb536901fd55ae0d64af533afd
TooBug <i@toobug.net>
2015-04-09 19:57:50 +0800

    登录界面以及用户Email是否注册查询

--- src/scripts/user.js
+++ src/scripts/user.js
@@ -1,9 +1,40 @@
+var remote = require('remote');
 var AV = require('avoscloud-sdk').AV;
 AV.initialize("pnj0o24lytzmcaoebtu3uoynwyuqqs687ch3nxpih0i45qid", "ce3286s9l40kyj5erom2sjlc22tyqku6tn3na3v8s6h17jrs");
 
+var queryUser = function(email, callback){
+	var query = new AV.Query(AV.User);
+	query.equalTo('username', email);
+	query.find({
+		success: function(users) {
+			callback(null, !!users.length);
+		},
+		error: function(users, err){
+			callback(err);
+		}
+	});
+};
+
+var queryUserCallback = function(err, result){
+	var isRegistered = true;
+	if(!err && !result){
+		isRegistered = false;
+	}
+	var ipc = require('ipc');
+	ipc.send('loginCallback.queryUser',isRegistered);
+};
+
+var bindEvents = function(){
+	var ipc = remote.require('ipc');
+	ipc.on('login.queryUser',function(e, email){
+		console.log('bindEvents');
+		queryUser(email, queryUserCallback);
+	});
+};
+
+
 exports.login = function(){
 
-	var remote = require('remote');
 	var BrowserWindow = remote.require('browser-window');
 	var loginWindow = new BrowserWindow({
 		width: 400,
@@ -15,5 +46,6 @@ exports.login = function(){
 		loginWindow.openDevTools();
 	}
 
+	bindEvents();
 };
 
