59befd2f70d604375470ca91a00377f2b6e0c1b9
TooBug <i@toobug.net>
2017-04-10 13:41:26 +0800

    新用户运行升级脚本出错的问题 close #10

--- src/docs/upgrade/scripts/0.3.0.js
+++ src/docs/upgrade/scripts/0.3.0.js
@@ -22,45 +22,51 @@ module.exports = function(isDebug){
 	let metaKey = `TooNote-LocalStorage-Key-${env}meta.json`;
 	console.log('[upgrade]env:%s', env);
 	console.log('[upgrade]准备读取meta');
-	let meta = JSON.parse(localStorage.getItem(metaKey));
-	console.log('[upgrade]读取meta成功');
+	let metaStr = localStorage.getItem(metaKey);
+	// 有可能是新安装的用户
+	if(!metaStr){
+		console.log('[upgrade]没有meta信息');
+	}else{
+		let meta = JSON.parse(localStorage.getItem(metaKey));
+		console.log('[upgrade]读取meta成功');
 
-	console.log('[upgrade]notebook -> notebooks');
-	meta.notebooks = meta.notebook;
-	delete meta.notebook;
+		console.log('[upgrade]notebook -> notebooks');
+		meta.notebooks = meta.notebook;
+		delete meta.notebook;
 
-	meta.notebooks.forEach(function(notebook){
-		console.log('[upgrade]notebook:%s', notebook.title);
-		console.log('[upgrade]增加notebook.createdAt');
-		notebook.createdAt = +notebook.id;
-		console.log('[upgrade]notebook.id -> uuid');
-		notebook.id = uuid.v4();
+		meta.notebooks.forEach(function(notebook){
+			console.log('[upgrade]notebook:%s', notebook.title);
+			console.log('[upgrade]增加notebook.createdAt');
+			notebook.createdAt = +notebook.id;
+			console.log('[upgrade]notebook.id -> uuid');
+			notebook.id = uuid.v4();
 
-		notebook.notes.forEach(function(note){
-			var tmpId = note.id;
-			console.log('[upgrade]note:%s', note.title);
-			console.log('[upgrade]增加note.pureTitle');
-			note.pureTitle = getPureTitle(note.title);
-			console.log('[upgrade]增加note.createdAt');
-			note.createdAt = +tmpId;
-			console.log('[upgrade]note.id -> uuid');
-			note.id = uuid.v4();
+			notebook.notes.forEach(function(note){
+				var tmpId = note.id;
+				console.log('[upgrade]note:%s', note.title);
+				console.log('[upgrade]增加note.pureTitle');
+				note.pureTitle = getPureTitle(note.title);
+				console.log('[upgrade]增加note.createdAt');
+				note.createdAt = +tmpId;
+				console.log('[upgrade]note.id -> uuid');
+				note.id = uuid.v4();
 
-			console.log('[upgrade]准备更新content存储');
-			var content = localStorage.getItem(`TooNote-LocalStorage-Key-note-${tmpId}.md`);
-			console.log('[upgrade]读取content成功');
-			localStorage.setItem(`TooNote-LocalStorage-Key-${env}note-${note.id}.md`, content);
-			console.log('[upgrade]写入新content成功');
-			localStorage.removeItem(`TooNote-LocalStorage-Key-${env}note-${tmpId}.md`);
-			console.log('[upgrade]删除旧content成功');
+				console.log('[upgrade]准备更新content存储');
+				var content = localStorage.getItem(`TooNote-LocalStorage-Key-note-${tmpId}.md`);
+				console.log('[upgrade]读取content成功');
+				localStorage.setItem(`TooNote-LocalStorage-Key-${env}note-${note.id}.md`, content);
+				console.log('[upgrade]写入新content成功');
+				localStorage.removeItem(`TooNote-LocalStorage-Key-${env}note-${tmpId}.md`);
+				console.log('[upgrade]删除旧content成功');
+
+			});
+
 
 		});
-
-
-	});
-	console.log('[upgrade]准备写入meta');
-	localStorage.setItem(metaKey, JSON.stringify(meta));
-	console.log('[upgrade]写入meta成功');
+		console.log('[upgrade]准备写入meta');
+		localStorage.setItem(metaKey, JSON.stringify(meta));
+		console.log('[upgrade]写入meta成功');
+	}
 
 	console.log('[upgrade]0.2.0 -> 0.3.0升级成功');
 
