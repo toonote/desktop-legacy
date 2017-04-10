e640594e182cb10d1668e17185f077bcf3d8d3b5
TooBug <i@toobug.net>
2017-04-10 21:24:44 +0800

    样式更新 + 更新打包脚本

--- dist.sh
+++ dist.sh
@@ -4,11 +4,11 @@ npm run build
 cd ..
 mkdir tmp
 mv src/{api,component,modules,models,vuex,*.map} tmp
-electron-packager src TooNote \
+ELECTRON_MIRROR=https://npm.taobao.org/mirrors/electron/ electron-packager src TooNote \
 	--platform=darwin \
 	--arch=x64 --version=1.3.8 \
 	--overwrite \
-	--app-version=0.2.0 \
+	--app-version=0.3.0 \
 	--icon=./logo/logo-new.icns \
 	# --ignore="(api\\\\(menu|store)\\\\[\\w]+|component\\\\[\\w]+\\.vue|modules\\\\[\\w]+|vuex\\\\store)"
 mv tmp/* src
