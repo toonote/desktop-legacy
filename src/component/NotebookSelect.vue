0708122e08691b246daa4429891c7ed8fb05a641
TooBug <i@toobug.net>
2017-11-25 16:50:51 +0800

    美化笔记选择界面

--- src/component/NotebookSelect.vue
+++ src/component/NotebookSelect.vue
@@ -1,22 +1,55 @@
 <style scoped>
 .notebookSelect{
 	position: absolute;
-	z-index: 2;
-	width: 800px;
-	height: 450px;
-	left: 50%;
-	top: 50%;
-	transform: translate(-50%, -50%);
-	background:white;
-	border:5px solid #bbb;
-	color:#585858;
+	left: 0;
+	top: 0;
+	width: 100%;
+	height: 100%;
+	background: #f0f0f0;
 	font-family: "PingFang SC";
+}
+.notebookSelect ul{
 	display: flex;
+	align-items: center;
+	width: 100%;
+	height: 100%;
+	justify-content: center;
+	box-sizing: border-box;
+	list-style: none;
+}
+.notebookSelect ul > li{
+	position: relative;
+	cursor: pointer;
+	box-sizing: border-box;
+	text-align: center;
+	padding-top: 160px;
+	width: 140px;
+	height: 200px;
+	background: linear-gradient(to bottom, #728B18, #728B18) no-repeat;
+	background-size: 100% 140px;
+	border-radius: 5px;
+	box-shadow: 0 0 8px rgba(0,0,0,.2);
+	text-shadow: 0 0 2px rgba(0,0,0,0.2);
+	color: #333;
+	transition: box-shadow .4s;
+}
+.notebookSelect ul > li:hover{
+	box-shadow: 0 0 15px rgba(0,0,0,.3);
+}
+.notebookSelect ul > li::after{
+	content: ' ';
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 140px;
+	background: url(../images/notebook-logo.png) no-repeat;
+	background-position: center center;
+	background-size: 70px 70px;
 }
 </style>
 
 <template>
-<section tabindex="1" class="mask">
 <section class="notebookSelect" v-on:click.stop>
 	<ul>
 		<li
@@ -26,7 +59,6 @@
 			@click="switchCurrentNotebook(notebook.id)"
 		>{{notebook.title}}</li>
 	</ul>
-</section>
 </section>
 </template>
 
@@ -43,7 +75,9 @@ export default {
 	watch: {
 	},
 	methods: {
+		switchCurrentNotebook(notebookId){
 
+		}
 	}
 };
 </script>
